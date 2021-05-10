ff8fb034212f9c3d78dec96dabd0c38fe1356b27
Stuart Wallace <stuart.wallace@opencredo.com>
2021-05-10 18:07:09 +0100

    Initial commit

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/opencredo/upcloudzerodowntime/go
+
+go 1.16
---
+++ go/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"html"
+	"net/http"
+	"fmt"
+	"log"
+)
+
+const Version = "1.0"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "(%s) Hello, %q", Version, html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
