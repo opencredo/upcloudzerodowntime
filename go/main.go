ab83f1f50a58569dce9ae047e10f3d6720a98d83
Stuart Wallace <stuart.wallace@opencredo.com>
2021-05-19 14:12:52 +0100

    Add artificial error to app

--- go/main.go
+++ go/main.go
@@ -5,12 +5,20 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"time"
 )
 
 const Version = "1.3"
 
 func main() {
+	startedTime := time.Now()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if time.Since(startedTime) < 2*time.Minute {
+			w.WriteHeader(500)
+			w.Write([]byte("Server Error"))
+			return
+		}
 		fmt.Fprintf(w, "(%s) Hello, %q", Version, html.EscapeString(r.URL.Path))
 	})
 
